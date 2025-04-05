8318f6cac8888f1ee71bafcf30c6e8860009d082
ragnarok0031 <daryljohanscastellonfranco@gmail.com>
2025-04-05 17:23:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
 document.querySelectorAll('button').forEach(btn => {
     btn.addEventListener('click', () => {
-        // Animación de escala al hacer clic en botones
+        // Animación de escala suave al hacer clic
         document.body.style.transition = 'transform 0.5s';
         document.body.style.transform = 'scale(0.9)';
         setTimeout(() => {
