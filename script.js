7db2ecead09e204891acec3dbf759d0626964093
ragnarok0031 <daryljohanscastellonfranco@gmail.com>
2025-04-06 20:54:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ function toggleMenu() {
     menu.classList.toggle('open');
 }
 
-// Menú responsive
-document.querySelector('.menu-toggle').addEventListener('click', toggleMenu);
+// Cerrar menú al hacer clic fuera
 document.addEventListener('click', (e) => {
     if (!e.target.closest('.menu-toggle') && !e.target.closest('.menu')) {
         document.querySelector('.menu').classList.remove('open');
@@ -29,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// Función para mapa
+// Función para mostrar el mapa
 function showMap() {
     window.open(
         "https://www.google.com/maps/place/UE+%22Guido+Arce+Pimentel%22/@-18.0653553,-64.1226254,1683m/data=!3m1!1e3!4m6!3m5!1s0x93f013006e9e0c51:0x5d3dfe25282e64aa!8m2!3d-18.0633661!4d-64.1212119!16s%2Fg%2F11ldr0xj4m?entry=ttu&g_ep=EgoyMDI1MDQwMi4xIKXMDSoASAFQAw%3D%3D",
