0ccdc27324fc6c065a27f6fd76074ff51a66f5eb
ragnarok0031 <daryljohanscastellonfranco@gmail.com>
2025-04-07 19:01:30 -0400

    Add files via upload

---
+++ createxyz-project/src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "c458da86-df9d-4c51-9208-e6a475bb44ed");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
